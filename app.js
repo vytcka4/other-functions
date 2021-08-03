7cfd7e7f2cabb7d7a219d818e32da913be0492fb
vytcka4 <vytautas.freelance@gmail.com>
2021-08-03 16:38:09 +0300

    commit

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 const butn = document.querySelector("button");
 
-butn.addEventListener("click", function () {});
+const greet = (greeting) => {
+  return (name) => {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+// greet("kietas")("vytcka");
+
+const poll = {
+  question: "What is your favourite programming language?",
+  options: ["0: JavaScript", "1: Python", "2: Rust", "3:  C++"],
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const answer = Number(
+      prompt(`${this.question}\n${this.options.join("\n")}`)
+    );
+    if (typeof answer === "number" && answer > 3) {
+      this.answers[answer]++;
+      console.log(this.answers);
+    }
+  },
+};
+
+butn.addEventListener("click", poll.registerNewAnswer.bind(poll));
